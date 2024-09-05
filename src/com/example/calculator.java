ec68330d26fe082c5acdd276ef40d42da2a524f3
정현우-07 <jhw8449@gmail.com>
2024-09-05 17:19:40 +0900

    박성규 튜터님과 공부 후 주석 수정

--- src/com/example/calculator.java
+++ src/com/example/calculator.java
@@ -20,11 +20,11 @@ public class calculator {
 
         int result = 0; // 구하려는 resuly 값을 초기화하여 에러를 방지
 
-        switch (c) {   //switch를 이용하여 내가 넣으려는 연산기호의 기능을 설정
+        switch (c) {   //switch를 이용하여 내가 넣으려는 연산기호의 기능을 설정, 조건문
             // switch, case를 이용하여 c에 들어간 연산기호를 찾을 수 있도록 설정
             case "+" :
-                result = a + b; // case 문을 통해 +를 찾았을 때 계산기의 +기능이 작동하도록 기능을 설정
-                break;  // 값을 다 찾았을 때 멈출 수 있도록 break를 사용
+                result = a + b; // case 문을 통해 +를 찾았을 때 계산기의 +기능이 작동하도록 기능을 설정 a + b라는 연산을 result에 담아준다.
+                break;  // 제일 가까운 자기의 부모를 멈춤, (여기선 switch를 멈춤)
             case "-" :
                 result = a - b;  // case 문을 통해 -를 찾았을 때 계산기의 -기능이 작동하도록 기능을 설정
                 break;
@@ -34,15 +34,14 @@ public class calculator {
             case "/" :
                 if (b != 0) {  // if문을 넣어서 b가 0이 아닐 때 result 값을 실행 할 수 있도록 함 true 일때 if문 실행
                     result = a / b;
-                } else {  // if문이 false일 때 else문을 실행하여 "0으로 나눌 수 없습니다"를 출력
+                } else {  // if문이 false일 때 else문을 실행하여 "0으로 나눌 수 없습니다"를 출력, if에서 주어진 조건이 아닐 때 else로 간다.
                     System.out.println("0으로 나눌 수 없습니다.");
                 }
                 break;
             default:  // 예외처리!! switch에 입력된 값을 case에서 찾지 못한 경우 default로 내려와 "잘못된 연산입니다"를 출력
                 System.out.println("잘못된 연산입니다");
-                return;
+                return;  // 여기서 멈추고 값을 주어라, break와 비슷한 결과, return은 값을 돌려주는 결과
         }
-
         System.out.println(result);
     }
 }
