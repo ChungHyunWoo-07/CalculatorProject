a0c17b2bae9f5ee51de13393355ad078a5b16c1b
정현우-07 <jhw8449@gmail.com>
2024-09-11 09:29:38 +0900

    getter, setter 작동안해서 삭제

--- src/com/example/App.java
+++ src/com/example/App.java
@@ -20,7 +20,7 @@ public class App { // 모든이름을 수정할 경우 단축키 F2를 눌러서
 
             Calculator calculator = new Calculator();  //Calculator클래스를 가져오기 위해 인스턴스화 하여 calculator(변수명)에 대입
             double result1 = 0;  // 계산하기 전에 result1의 값을 0으로 초기화
-            result1 = calculator.getCalculate(n1, n2, p);  // 런타임 후 작성되는 값으로 실행하기 위해 지정한 변수명을 넣어 스캐너를 통해 읽음
+            result1 = calculator.calculate(n1, n2, p);  // 런타임 후 작성되는 값으로 실행하기 위해 지정한 변수명을 넣어 스캐너를 통해 읽음
             System.out.println("결과: " + result1);  // 결과값 도출
 
             System.out.print("계산을 종료하시려면 exit를 입렵하세요: ");  // 결과값 도출 후 exit외에 다른 문자열을 넣으면 다시 while문 반복
--- src/com/example/Calculator.java
+++ src/com/example/Calculator.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Calculator {
     private double calculate;
 
-    private double calculate(int num1, int num2, String operator) {
+    public double calculate(int num1, int num2, String operator) {
         double result = 0.0;
         while (true) {
             switch (operator) {
@@ -31,10 +31,5 @@ public class Calculator {
             }
             return result;
         }
-    }
-
-    public double getCalculate(int n1, int n2, String p) {
-        return calculate;  // 강의보면서 작성했는데 인텔리제이에서 오류 부분 수정해줌
-        // 작동원리 다시 파악 필요, App클래스의 getCalculate도 마찬가지
     }
 }
